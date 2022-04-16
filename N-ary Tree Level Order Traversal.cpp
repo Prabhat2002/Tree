8b184a8d320e417bc01b69edc234783623526137
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-04-16 12:10:11 +0530

    Create N-ary Tree Level Order Traversal.cpp

---
+++ N-ary Tree Level Order Traversal.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    vector<vector<int>> res;
+    void rec(Node* root, int level) 
+    {
+        if (!root) 
+            return;
+        if (level == res.size()) 
+            res.push_back({});
+        res[level].push_back(root->val);
+        for (auto child : root->children)
+            rec(child, level+1);
+    }
+    
+    vector<vector<int>> levelOrder(Node* root) 
+    {
+        rec(root, 0);
+        return res;
+    }
+};
